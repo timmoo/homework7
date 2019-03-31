1ff17c8686995bb9d4228be35fc9e0639f8ee086
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-31 12:42:24 +0300

    Update SQLQuery7-1.sql

--- SQLQuery7-1.sql
+++ SQLQuery7-1.sql
@@ -5,48 +5,52 @@ BEGIN
 DROP TABLE #TempTable
 end;
 
-set statistics time on
-select 
+SET STATISTICS TIME ON
+SELECT
 IL.InvoiceID,
 I.InvoiceDate,
 C.CustomerName,
-IL.Quantity*IL.UnitPrice as [Sale Amount],
-(select sum(ILInner.Quantity*ILInner.UnitPrice)
-from [Sales].[InvoiceLines] as ILInner
-join [Sales].[Invoices] as IInner on IInner.InvoiceID = ILInner.InvoiceID
-where IInner.InvoiceDate >= '2015-01-01' and IInner.InvoiceDate <= eomonth(I.InvoiceDate)
-) as [Sale Amount by Month]
-into #TempTable
-from [Sales].[InvoiceLines] as IL
-join [Sales].[Invoices] as I on I.InvoiceID = IL.InvoiceID
-join [Sales].[Customers] as C on C.CustomerID = I.CustomerID
-
-SELECT * from #TempTable
-where #TempTable.InvoiceDate >= '2015-01-01'
-order by #TempTable.InvoiceDate, [Sale Amount], #TempTable.InvoiceID, #TempTable.CustomerName;
+IL.Quantity*IL.UnitPrice AS [Sale Amount],
+(SELECT SUM(ILInner.Quantity*ILInner.UnitPrice)
+FROM [Sales].[InvoiceLines] AS ILInner
+JOIN [Sales].[Invoices] AS IInner ON IInner.InvoiceID = ILInner.InvoiceID
+WHERE IInner.InvoiceDate >= '2015-01-01' AND IInner.InvoiceDate <= EOMONTH(I.InvoiceDate)) AS [Sale Amount by Month]
+INTO #TempTable
+FROM [Sales].[InvoiceLines] AS IL
+JOIN [Sales].[Invoices] AS I ON I.InvoiceID = IL.InvoiceID
+JOIN [Sales].[Customers] AS C ON C.CustomerID = I.CustomerID
+SELECT * FROM #TempTable
+WHERE #TempTable.InvoiceDate >= '2015-01-01'
+ORDER BY #TempTable.InvoiceDate, [Sale Amount], #TempTable.InvoiceID, #TempTable.CustomerName;
 
 
 --Table Variable
-DECLARE @TableVar TABLE(InvoiceId INT NOT NULL,
-							InvoiceDate DATE NOT NULL,
-								CustomerName NVARCHAR(100) NOT NULL,
-									SaleAmount Decimal(18,2) NOT NULL,
-										TotalSumm Decimal(18,2) NOT NULL )
-								
-INSERT INTO @TableVar (InvoiceId,
-							InvoiceDate,
-								CustomerName,
-									SaleAmount,
-										TotalSumm)
+DECLARE @TableVar 
+TABLE
+(
+InvoiceId INT NOT NULL,
+InvoiceDate DATE NOT NULL,
+CustomerName NVARCHAR(100) NOT NULL,
+SaleAmount Decimal(18,2) NOT NULL,
+TotalSumm Decimal(18,2) NOT NULL 
+)			
+INSERT INTO @TableVar 
+(
+InvoiceId,
+InvoiceDate,
+CustomerName,
+SaleAmount,
+TotalSumm
+)
 SELECT 
 I.InvoiceID, 
 S.InvoiceDate, 
 C.CustomerName,
-I.Quantity*I.UnitPrice as [Sale Amount],
+I.Quantity*I.UnitPrice AS [Sale Amount],
 SUM(I.Quantity*I.UnitPrice) OVER (ORDER BY YEAR(S.InvoiceDate), MONTH(S.InvoiceDate) RANGE UNBOUNDED PRECEDING) AS TotalSumm  		
 FROM Sales.Invoices AS S
 JOIN Sales.InvoiceLines AS I ON S.InvoiceID=I.InvoiceID
 JOIN Sales.Customers AS C ON S.CustomerID=C.CustomerID
-where InvoiceDate >= '2015-01-01' and InvoiceDate <= eomonth(InvoiceDate)
-SELECT * from @TableVar
-order by InvoiceDate, SaleAmount, InvoiceID, CustomerName;
+WHERE InvoiceDate >= '2015-01-01' AND InvoiceDate <= EOMONTH(InvoiceDate)
+SELECT * FROM @TableVar
+ORDER BY InvoiceDate, SaleAmount, InvoiceID, CustomerName;
